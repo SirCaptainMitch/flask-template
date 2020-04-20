0e951cadb0d9746a243abd72f1738d1cd8e3c75c
SirCaptainMitch <hamann.mitchell@gmail.com>
2020-04-19 19:59:06 -0500

    adding store

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
 
 from app.security import authenticate, identity
-from app.user import UserRegister
-from app.item import Item, ItemList
+from app.resources.user import UserRegister
+from app.resources.item import Item, ItemList
+from app.resources.store import Store, StoreList
+from app.db import db 
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +16,14 @@ jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
+@app.before_first_request
+def create_tables():
+  db.create_all()
+
+db.init_app(app)
